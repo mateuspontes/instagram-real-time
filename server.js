e91f38aacaa6475388f98427a9cccef836ea1623
Mateus <mateuspo10@gmail.com>
2014-07-18 23:33:35 -0300

    Troca tag para #beach

--- server.js
+++ server.js
@@ -12,22 +12,6 @@ var pub = __dirname + '/public',
 
 var clientID = 'df8f2e7775d641ea85f6978c799c411b',
     clientSecret = 'dc9a9281ea8c49bf973f2a98576d7934';
-/**
- * Set the configuration
- */
-Instagram.set('client_id', clientID);
-Instagram.set('client_secret', clientSecret);
-Instagram.set('callback_url', 'http://festpizza.herokuapp.com/callback');
-Instagram.set('redirect_uri', 'http://festpizza.herokuapp.com');
-Instagram.set('maxSockets', 10);
-
-Instagram.subscriptions.subscribe({ object: 'tag',  object_id: 'brasil' });
-
-
-//https://api.instagram.com/v1/subscriptions?client_secret=dc9a9281ea8c49bf973f2a98576d7934&client_id=df8f2e7775d641ea85f6978c799c411b
-//curl -X DELETE 'https://api.instagram.com/v1/subscriptions?client_secret=dc9a9281ea8c49bf973f2a98576d7934&object=all&client_id=df8f2e7775d641ea85f6978c799c411b'
-// Instagram.subscriptions.unsubscribe({ id: '3668016' });
-// Instagram.subscriptions.unsubscribe({ id: '8158759' });
 
 io.configure(function () {
   io.set("transports", [
@@ -40,29 +24,39 @@ io.configure(function () {
   io.set("polling duration", 10);
 });
 
-/**
- * Set your app main configuration
- */
-app.configure(function(){
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(pub));
-    app.use(express.static(view));
-    app.use(express.errorHandler());
-});
+Instagram.set('client_id', clientID);
+Instagram.set('client_secret', clientSecret);
+Instagram.set('callback_url', 'http://festpizza.herokuapp.com/callback');
+Instagram.set('redirect_uri', 'http://festpizza.herokuapp.com');
+Instagram.set('maxSockets', 10);
 
-app.get("/views", function(req, res){
-    res.render("index");
-});
+Instagram.subscriptions.subscribe({ object: 'tag',  object_id: 'beach' });
 
 io.sockets.on('connection', function (socket) {
   Instagram.tags.recent({
-      name: 'brasil',
-      complete: function(data) {
-        socket.emit('firstShow', { firstShow: data });
-      }
+    name: 'beach',
+    complete: function(data) {
+      socket.emit('firstShow', { firstShow: data });
+    }
   });
+});
+
+//https://api.instagram.com/v1/subscriptions?client_secret=dc9a9281ea8c49bf973f2a98576d7934&client_id=df8f2e7775d641ea85f6978c799c411b
+//curl -X DELETE 'https://api.instagram.com/v1/subscriptions?client_secret=dc9a9281ea8c49bf973f2a98576d7934&object=all&client_id=df8f2e7775d641ea85f6978c799c411b'
+// Instagram.subscriptions.unsubscribe({ id: '3668016' });
+// Instagram.subscriptions.unsubscribe({ id: '8158759' });
+
+app.configure(function(){
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(pub));
+  app.use(express.static(view));
+  app.use(express.errorHandler());
+});
+
+app.get("/views", function(req, res){
+  res.render("index");
 });
 
 app.get('/callback', function(req, res){
